20d539f1448a2ef23cb29b41456357aa17557d2e
Hamzeh <hamzehx2@outlook.com>
2023-07-08 01:16:11 +0200

    Created scripts for cannon and mage, still need to change behavior

---
+++ Assets/Scripts/Towers/MageTower.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MageTower : Tower
+{
+    // Start is called before the first frame update
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        attackTimer -= Time.deltaTime;
+        if (attackTimer < 0)
+        {
+            attackTimer = 1 / attackSpeed;
+            if (inRange.Count > 0)
+            {
+                // still need to change attack behaviour and number of targets
+                getClosestTarget().GetComponent<Attackable>().Attacked(damage, false, true);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Entered");
+        if (collision.gameObject.GetComponent<Attackable>() != null)
+        {
+            inRange.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Attackable>() != null)
+        {
+            inRange.Remove(collision.gameObject);
+        }
+    }
+
+    // returns the closest gameObject in inRange to this tower
+    private GameObject getClosestTarget()
+    {
+        GameObject target = inRange[0];
+        float minDistace = Vector3.Distance(gameObject.transform.position, target.transform.position);
+        foreach (GameObject g in inRange)
+        {
+            float gDistance = Vector3.Distance(gameObject.transform.position, g.transform.position);
+            if (minDistace > gDistance)
+            {
+                target = g;
+                minDistace = gDistance;
+            }
+        }
+        return target;
+    }
+}
