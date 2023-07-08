6c2986071013dcd6a3f5b9ca8f27bb30c6b2c89a
DieserMastro <117863339+DieserMastro@users.noreply.github.com>
2023-07-08 13:16:26 +0200

    Updating colliders

--- Assets/Scripts/Mobs/Speedster.cs
+++ Assets/Scripts/Mobs/Speedster.cs
@@ -161,6 +161,18 @@ public class Speedster : Mob
         //Optional Death animation lol
         Destroy(gameObject);
     }
+    public override void OnUnitCollision(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Soldier"))
+        {
+            initiateCombat(collision.gameObject);
+        }
+    }
+
+    public override void initiateCombat(GameObject soldier)
+    {
+        throw new System.NotImplementedException();
+    }
     #endregion Game Mechanics / Methods
 
     #region Overarching Methods / Helpers
