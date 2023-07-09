2527362ce857fc5f0781e6ca7a4847e5ed1f735d
Hyun Bin <jeounghyunbin@gmail.com>
2023-07-09 17:43:37 +0200

    Update UIManagerScript.cs

--- Assets/WaveMaker/UIManagerScript.cs
+++ Assets/WaveMaker/UIManagerScript.cs
@@ -117,7 +117,6 @@ public class UIManagerScript : MonoBehaviour
                     currentSlotIndex++;
                 }
                 break;
-                break;
             default:
 
                 break;
