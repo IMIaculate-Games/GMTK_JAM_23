5e64ef7246f3fc86e0ecf02fae08897fa64ca131
Hamzeh <hamzehx2@outlook.com>
2023-07-09 11:58:36 +0200

    changed mob scripts

--- Assets/Mob/EnemyMovement.cs
+++ Assets/Mob/EnemyMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private Rigidbody2D myRigidbody;
+    //private Rigidbody2D myRigidbody;
 
-    [SerializeField] private float moveSpeed = 2f; //The Speed defined by the Enemy speed property
+    private float moveSpeed = 2f; //The Speed defined by the Enemy speed property
 
     [SerializeField] private Mob mob;
 
@@ -22,60 +22,71 @@ public class EnemyMovement : MonoBehaviour
     void Start()
     {
         target = LevelManager.main.path[Index];
-        myRigidbody = GetComponent<Rigidbody2D>();
+        //myRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         moveSpeed = mob.MovementSpeed;
+
+        updateAnimation();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(target.position, transform.position) <= 0.1f)
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
         {
             Index++;
 
-            if(Index == LevelManager.main.path.Length)
+            if (Index == LevelManager.main.path.Length)
             {
                 Destroy(gameObject);
                 return;
-            } else
-            {
-                target = LevelManager.main.path[Index];
-
-                
-            }
-
-            Vector3 animDirection = Vector3.Normalize(target.position - transform.position);
-            if (Mathf.Abs(animDirection.x) > Mathf.Abs(animDirection.y))
-            {
-                if (animDirection.x < 0)
-                {
-                    //walk left
-                    animator.Play("walk_left");
-                }
-                else
-                {
-                    //walk right
-                    animator.Play("walk_right");
-                }
             }
             else
             {
-                if (animDirection.y < 0)
-                {
-                    //walk down
-                    animator.Play("walk_down");
-                }
-                else
-                {
-                    //walk up
-                    animator.Play("walk_up");
-                }
+                target = LevelManager.main.path[Index];
+
+                updateAnimation();
+
             }
+
         }
+
+            
 
         Vector3 direction = Vector3.Normalize(target.position - transform.position);
 
-        myRigidbody.velocity = moveSpeed * Time.deltaTime * direction;
+        //myRigidbody.velocity = moveSpeed * Time.deltaTime * direction;
+        transform.Translate(moveSpeed * Time.deltaTime * direction / 100);
+    }
+
+    private void updateAnimation()
+    {
+        Vector3 animDirection = Vector3.Normalize(target.position - transform.position);
+        if (Mathf.Abs(animDirection.x) > Mathf.Abs(animDirection.y))
+        {
+            if (animDirection.x < 0)
+            {
+                //walk left
+                animator.Play("walk_left");
+            }
+            else
+            {
+                //walk right
+                animator.Play("walk_right");
+            }
+        }
+        else
+        {
+            if (animDirection.y < 0)
+            {
+                //walk down
+                animator.Play("walk_down");
+            }
+            else
+            {
+                //walk up
+                animator.Play("walk_up");
+            }
+        }
     }
 }
--- Assets/Mob/Speedster.cs
+++ Assets/Mob/Speedster.cs
@@ -1,91 +1,23 @@
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// TODO: Provide a summary of your script here.
-/// To create an auto-generated summary template
-/// type 3 /// slash characters after you have
-/// written your class or method signature.
-/// </summary>
+
 public class Speedster : Mob
 {
-    /**
-        * TODO: General Structure Ideas:
-        * 
-        * Try to keep an order of fields from most complex to primitive.
-        * GameObject go;
-        * struct point;
-        * float num;
-        * bool truthy;
-        * 
-        * Constants before variables maybe too.
-        * const int TIME_PLANNED_FOR_THIS
-        * int timeSpentOnThis
-        * 
-        * Also from most public to private. Valid for methods too.
-        * public
-        * internal
-        * protected
-        * private
-        * 
-        *  Then only probably by alphabet. If at all
-        */
-
-    #region Serialized Fields
-
-    //public MobData data;
-
-
-
-    #endregion Serialized Fields
-
-    #region Fields
-
     private Animator animator;
-    // TODO: Put general non-serialized fields here.
-
-
-    #endregion Fields
-
-    #region Built-Ins / MonoBehaviours
-
-    // TODO: Put Unity built-in event methods here.
-    // Such as Awake, Start, Update.
-    // But also OnEnable, OnDestroy, OnTrigger and such.
-
+   
     void Awake()
     {
-        Initialize();
+        base.Initialize();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    #endregion Built-Ins / MonoBehaviours
-
-    #region GetSets / Properties
-
-    #endregion GetSets / Properties
-
-    #region Game Mechanics / Methods
-
-    // TODO: Put your game specific mechanics here.
-    // If they can be grouped by functionality, do so.
-
-    /// <summary>
-    /// TODO: Provide a summary for the method
-    /// </summary>
-    /// <param name="param">List the parameters.</param>
-    /// <returns>Specify what it returns, if it does so.</returns>
-
     public override void Attacked(int damage, bool isMagic, bool isRanged)
     {
-
         if (isMagic)
         {
             TakeDamage(damage);
@@ -110,10 +42,8 @@ public class Speedster : Mob
 
         }
         TakeDamage(damage - resistance);
-
-
-        //throw new NotImplementedException();
     }
+
     public override void TakeDamage(int damage)
     {
         if (healthPoints == 0)
@@ -134,12 +64,4 @@ public class Speedster : Mob
         DropMoney();
         Destroy(gameObject);
     }
-    
-    #endregion Game Mechanics / Methods
-
-    #region Overarching Methods / Helpers
-
-    // TODO: Put your helper methods here.
-
-    #endregion Overarching Methods / Helpers
 }
